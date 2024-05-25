2b970912d32b3afe775f6639ed12276a80228ca8
WumboWill <william.zycinsky@gmail.com>
2024-05-25 12:38:45 -0400

    Added  Major Components of Library

---
+++ CaptainNeckbeard/src/app/library/comic-results/comic-results.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comic-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './comic-results.component.html',
+  styleUrl: './comic-results.component.css'
+})
+export class ComicResultsComponent {
+
+}
